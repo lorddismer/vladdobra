b5b7d7dabb34dd50e13f5cb730aca47c2a1d2792
lorddismer <63313026+lorddismer@users.noreply.github.com>
2020-04-10 13:07:56 +0200

    Delete script.js

--- css/script.js
+++
@@ -1,7 +0,0 @@
-let page = document.querySelector(".wrapper");
-let themeButton = document.querySelector(".theme-button");
-
-themeButton.onclick = function () {
-  page.classList.toggle("dark-theme");
-  page.classList.toggle("light-theme");
-};
